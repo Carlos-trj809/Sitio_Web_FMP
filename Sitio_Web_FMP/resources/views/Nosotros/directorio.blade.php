91e59b59911de1a33749bd33e284fb09ccd739ae
Carlos-trj809 <at14003@ues.edu.sv>
2021-04-28 00:24:32 -0600

    agregado directorio

---
+++ Sitio_Web_FMP/resources/views/Nosotros/directorio.blade.php
@@ -0,0 +1,18 @@
+@extends('pagina/baseOnlyHtml')
+
+@section('container')
+<div class="wrapper">
+    <div class="container-fluid">
+
+        <!-- start page title -->
+        <div class="page-title-alt-bg color-top"></div>
+        <div class="page-title-box color-boton py-2 rounded">
+            <h2 class="page-title text-white">Facultad Multidisciplinaria Paracentral</h2>
+        </div> 
+        </br>
+        <!-- end page title -->          
+        
+        
+    </div> <!-- end container -->
+</div> 
+@endsection
